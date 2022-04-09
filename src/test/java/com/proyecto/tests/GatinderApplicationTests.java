70e21d7a511e78badfd5602de5ca79226a5bdba1
aitormv <80701156+aitormv@users.noreply.github.com>
2022-04-09 15:18:07 +0200

    prueba

--- src/test/java/com/proyecto/tests/GatinderApplicationTests.java
+++ src/test/java/com/proyecto/tests/GatinderApplicationTests.java
@@ -1,26 +1,14 @@
 package com.proyecto.tests;
 
 import org.junit.jupiter.api.Test;
-import org.junit.Assert;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import com.proyecto.servicio.ServicioUsuario;
 
 @SpringBootTest
 class GatinderApplicationTests {
 	
-	@Autowired
-	ServicioUsuario su;
-
 	@Test
 	public void test01() {
-		Assert.assertNotNull(su.findByNombreUsuario("aitormv"));
-	}
-	
-	@Test
-	public void test02() {
-		Assert.assertNotNull(su.loadUserByUsername("aitormv"));
+		
 	}
 
 }
